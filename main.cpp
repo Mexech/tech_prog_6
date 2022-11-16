455a58886888a3eff2d9f2ea7e68575edbbc8feb
Mexech <makspopov11@mail.ru>
2022-11-16 16:26:03 +0300

    Ready

--- main.cpp
+++ main.cpp
@@ -32,6 +32,7 @@
 не содержащие запятых.
 */
 #include <iostream>
+#include <fstream>
 #include "Keeper.h"
 
 using namespace std;
@@ -44,16 +45,55 @@ void dispWorkersMenu() {
     cout << "Type command:\n1 - add new worker\n2 - display workers\n3 - edit worker\n4 - remove worker\n5 - display workers with specified work experience\n6 - exit\n";
 }
 
-void dispFilesTask() {
-    cout << "" << endl;
+void dispFilesTaskMenu() {
+    cout << "Type command:\n1 - display sentences without commas from file\n2 - display sentences without commas from string\n3 - exit" << endl;
+}
+
+void displayWithoutCommas(string line) {
+    bool has_commas = false;
+    string s, res;
+    for(char& c : line) {
+        s += c;
+        if (c == '.') {
+            if (!has_commas)
+                res += s;
+            has_commas = false;
+            s = "";
+        } else if (c == ',') {
+            has_commas = true;
+        }
+    }
+    if (res.length())
+        cout << res << endl;
+    else cout << endl;
+}
+
+void displayWithoutCommas() {
+    fstream in("text.txt", fstream::in);
+    string line, s;
+    bool has_commas = false;
+    while (getline(in, line)) {
+        for(char& c : line) {
+            s += c;
+            if (c == '.') {
+                if (!has_commas) 
+                    cout << s;
+                has_commas = false;
+                s = "";
+            } else if (c == ',') {
+                has_commas = true;
+            }
+        }
+        s += '\n';
+    }
+    cout << endl;
 }
 
 int main() {
     Keeper k;
     while (1) {
         dispMenu();
-        // int num; cin >> num;
-        int num = 1;
+        int num; cin >> num;
         if (num == 1) {
             while (1) {
                 dispWorkersMenu();
@@ -93,7 +133,22 @@ int main() {
                 }
             }
         } else if (num == 2) {
-            // TODO
+            while (1) {
+                dispFilesTaskMenu();
+                int num; cin >> num;
+                if (num == 1) {
+                    displayWithoutCommas();
+                } else if (num == 2) {
+                    cout << "Specify string:\n";
+                    cin.ignore();
+                    string s; getline(cin, s);
+                    displayWithoutCommas(s);
+                } else if (num == 3) {
+                    break;
+                } else {
+                    cout << "No such command. Try again." << endl;    
+                }
+            }
         } else {
             cout << "No such command. Try again." << endl;
         }
